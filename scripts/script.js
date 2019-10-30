7012d1db991ebaf289e3746b263cd208473eef9c
TekMal <k.kochanowska6@gmail.com>
2019-10-30 18:32:55 +0100

    show posters (only ten results)

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-fetch('http://www.omdbapi.com/?type=series&s=friends&plot=full&r=json&apikey=6b6ec75b')
+fetch('http://www.omdbapi.com/?type=series&s=friends&plot=full&r=json&page=5&apikey=6b6ec75b')
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -6,10 +6,26 @@ fetch('http://www.omdbapi.com/?type=series&s=friends&plot=full&r=json&apikey=6b6
         return;
       }
       response.json().then(function(data) {
-        console.log(data);
+          console.log(data)
+            //showData(data['Search']);
       });
     }
   )
   .catch(function(err) {
     console.log('Error: ', err);
   });
+
+  function showData(data){
+        let box = document.getElementById("series_container")
+        data.forEach(item => {
+            let image = document.createElement("img")
+            image.className = 'poster'
+            if(item.Poster !== "N/A"){
+                image.src = item.Poster}
+            else{
+                image.src = "https://images.unsplash.com/photo-1497514440240-3b870f7341f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=281&q=80"
+            }
+        box.appendChild(image)
+      })
+  }
+
