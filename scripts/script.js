6458602dd6ffe376d81edbb36d579dd31585b0b1
TekMal <k.kochanowska6@gmail.com>
2019-10-31 12:42:12 +0100

    add first search wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,36 @@
-fetch('http://www.omdbapi.com/?type=series&s=friends&plot=full&r=json&page=28&apikey=6b6ec75b')
-.then(
+let searchButton = document.getElementById('find')
+searchButton.addEventListener('click', searchSeries)
+
+function searchSeries(){
+    let inputSearch = (document.getElementById('series-search').value).toString()
+    fetch(`http://www.omdbapi.com/?type=series&s=${inputSearch}&plot=full&r=json&apikey=6b6ec75b`)
+    .then(
     function(response) {
       if (response.status !== 200) {
         console.log(response.status);
         return;
       }
       response.json().then(function(data) {
-          createUrlForPages(Math.ceil(data['totalResults'] / 10))
+          createUrlForPages(Math.ceil(data['totalResults'] / 10), inputSearch)
       });
     }
   )
   .catch(function(err) {
     console.log('Error: ', err);
   });
+}
 
-function createUrlForPages(pages){
+
+function createUrlForPages(pages, title){
     let urlsForPages = []
     for(let i=1; i<=pages; i++){
-        urlsForPages.push(fetch(`http://www.omdbapi.com/?type=series&s=friends&plot=full&r=json&page=${i}&apikey=6b6ec75b`))
+        urlsForPages.push(fetch(`http://www.omdbapi.com/?type=series&s=${title}&plot=full&r=json&page=${i}&apikey=6b6ec75b`))
     }
     Promise.all(urlsForPages).then(response => response.forEach(response => response.json().then(data => showData(data['Search']))))
 } 
 
   function showData(data){
-        let box = document.getElementById("series_container")
+        let box = document.getElementById("series-container")
         data.forEach(item => {
             let image = document.createElement("img")
             image.className = 'poster'
