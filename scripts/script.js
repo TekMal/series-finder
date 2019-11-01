68da2ec6b470495857438fe130b7bed024d909cd
TekMal <k.kochanowska6@gmail.com>
2019-11-01 12:38:09 +0100

    add title

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,7 @@ function searchSeries(){
             getTotalResults(Math.ceil(data['totalResults'] / 10), inputSearch)
           }
           else{
-            noResults()
+            noResults('No results')
           }
       });
     }
@@ -26,42 +26,50 @@ function searchSeries(){
   });
 }
 
-function noResults(){
-    let box = document.getElementById("series-container")
-    let info = createElementWithClassname('h2', 'info-no-results')
-    info.innerHTML = "No results"
-    box.appendChild(info)
-}
-
-
 function getTotalResults(pages, title){
     let urlsForPages = []
     for(let i=1; i<=pages; i++){
         urlsForPages.push(fetch(`http://www.omdbapi.com/?type=series&s=${title}&plot=full&r=json&page=${i}&apikey=6b6ec75b`))
     }
     Promise.all(urlsForPages).then(response => response.forEach(response => response.json().then(data => showData(data['Search']))))
-} 
+}
 
-  function showData(data){
-        let box = document.getElementById("series-container")
-        data.forEach(item => {
-            let image = createElementWithClassname('img', 'poster')
-            if(item.Poster !== "N/A"){
-                image.src = item.Poster}
-            else{
-                image.src = "https://images.unsplash.com/photo-1497514440240-3b870f7341f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=281&q=80"
-            }
-        box.appendChild(image)
-      })
-  }
+function showData(data){
+    let box = document.getElementById("series-container")
+    data.forEach(item => {
+    createPoster(item, box)
+    createTitle(item, box)
+  })
+}
 
+function createPoster(item, parent){
+    let image = createElementWithClassname('img', 'poster')
+        if(item.Poster !== "N/A"){
+            image.src = item.Poster}
+        else{
+            image.src = "https://images.unsplash.com/photo-1497514440240-3b870f7341f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=281&q=80"
+        }
+    parent.appendChild(image)
+}
+function createTitle(item, parent){
+    let title = createElementWithClassname('h2', 'series-title')
+    title.innerHTML = item.Title
+    parent.appendChild(title)
+}
 
-  function clearBox(elementID)
-{
+function clearBox(elementID){
     document.getElementById(elementID).innerHTML = "";
 }
+
 function createElementWithClassname(elementTag, elementClassName){
     let element = document.createElement(elementTag)
     element.className = elementClassName
     return element
+}
+
+function noResults(content){
+    let box = document.getElementById("series-container")
+    let info = createElementWithClassname('h2', 'info-no-results')
+    info.innerHTML = content
+    box.appendChild(info)
 }
