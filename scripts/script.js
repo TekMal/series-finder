fb5d3c6370a889c770a49c1ca96037444c7facb8
TekMal <k.kochanowska6@gmail.com>
2019-11-01 15:30:29 +0100

    display details

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const searchButton = document.getElementById('find')
 const mainSection = document.getElementById("series-container")
+const plotLength = 100
 searchButton.addEventListener('click', searchSeries)
 
 function searchSeries(){
@@ -26,28 +27,56 @@ function searchSeries(){
     console.log('Error: ', err)
   })
 }
-
 function getTotalResults(pages, title){
     const urlsForPages = []
     for(let i=1; i<=pages; i++){
         urlsForPages.push(fetch(`http://www.omdbapi.com/?type=series&s=${title}&r=json&page=${i}&apikey=6b6ec75b`))
     }
-    Promise.all(urlsForPages).then(response => response.forEach(response => response.json().then(data => showData(data['Search']))))
+    Promise.all(urlsForPages).then(response => response.forEach(response => response.json().then(data => getAllData(data['Search']))))
 }
 
-function showData(data){
+function getAllData(data){
     data.forEach(item => {
-    mainSection.appendChild(createSingleSeriesItem(item))
-
+        getSingleSeriesById(item.imdbID)
+      })
+}
+const allResults = []
+function getSingleSeriesById(id){
+    fetch(`http://www.omdbapi.com/?type=series&i=${id}&plot=short&r=json&apikey=6b6ec75b`)
+    .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log(response.status)
+      }
+      response.json().then(function(data) {
+          if(data['Response'] === 'True'){
+            mainSection.appendChild(createSingleSeriesItem(data))
+            allResults.push(data)
+            console.log(allResults)
+          }
+          else{
+            console.log("no result response : false")
+          }
+      })
+    }
+  )
+  .catch(function(err) {
+    console.log('Error: ', err)
   })
 }
 
 function createSingleSeriesItem(item){
     const seriesBox = createElementWithClassname('div', 'single-series')
-    seriesBox.appendChild(createPoster(item))
-    seriesBox.appendChild(createTitle(item))
-    seriesBox.appendChild(createDate(item))
-    seriesBox.appendChild(getSingleSeriesById(item.imdbID))
+    const poster = createElementWithClassname('div', 'poster-box')
+    const description = createElementWithClassname('div', 'description')
+    poster.appendChild(createPoster(item))
+    description.appendChild(createTitle(item))
+    description.appendChild(createDate(item))
+    description.appendChild(createRuntime(item))
+    description.appendChild(createRating(item))
+    description.appendChild(createPlot(item))
+    seriesBox.appendChild(poster)
+    seriesBox.appendChild(description)
     return seriesBox
 }
 
@@ -61,7 +90,7 @@ function createPoster(item){
     return image
 }
 function createTitle(item){
-    const title = createElementWithClassname('h2', 'series-title')
+    const title = createElementWithClassname('h2', 'title')
     title.innerHTML = item.Title
     return title
 }
@@ -74,17 +103,33 @@ function createDate(item){
         }
     return year
 }
-function createRuntime(itemRuntime){
-    console.log(itemRuntime)
+function createRuntime(item){
     const runtime = createElementWithClassname('p', 'runtime')
-    if(itemRuntime !== "N/A"){
-        runtime.innerHTML = itemRuntime}
+    if(item.Runtime !== "N/A"){
+        runtime.innerHTML = item.Runtime}
     else{
         runtime.innerHTML = "unknown"
     }
     return runtime
 }
-
+function createRating(item){
+    const rating = createElementWithClassname('p', 'rating')
+    if(item.Ratings.length !== 0){
+        rating.innerHTML = item.Ratings[0].Value}
+    else{
+        rating.innerHTML = "unknown"
+    }
+    return rating
+}
+function createPlot(item){
+    const plot = createElementWithClassname('p', 'plot')
+    if(item.Plot !== "N/A"){
+        plot.innerHTML = item.Plot.substring(0, plotLength)}
+    else{
+        plot.innerHTML = "unknown"
+    }
+    return plot
+}
 
 function clearBox(elementID){
     document.getElementById(elementID).innerHTML = ""
@@ -101,26 +146,4 @@ function noResults(content){
     const info = createElementWithClassname('h2', 'info-no-results')
     info.innerHTML = content
     box.appendChild(info)
-}
-
-function getSingleSeriesById(id){
-    fetch(`http://www.omdbapi.com/?type=series&i=${id}&plot=short&r=json&apikey=6b6ec75b`)
-    .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log(response.status)
-      }
-      response.json().then(function(data) {
-          if(data['Response'] === 'True'){
-            return createRuntime(data.Runtime)
-          }
-          else{
-            console.log("no result response : false")
-          }
-      })
-    }
-  )
-  .catch(function(err) {
-    console.log('Error: ', err)
-  })
 }
